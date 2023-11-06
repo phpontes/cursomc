7bdc4db03769fa10dce9ab2e5c49cbafbed73006
phpontes <ppontes95@hotmail.com>
2023-11-06 09:32:10 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/paulohenrique/cursomc/domain/Pagamento.java
+++ src/main/java/com/paulohenrique/cursomc/domain/Pagamento.java
@@ -3,6 +3,7 @@ package com.paulohenrique.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.paulohenrique.cursomc.domain.enums.EstadoPagamento;
 
 import jakarta.persistence.Entity;
@@ -22,6 +23,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 	
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
